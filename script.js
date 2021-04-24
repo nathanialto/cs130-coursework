c1eca03096401529c5c763d67e676168537abe64
nathanialto <67659961+nathanialto@users.noreply.github.com>
2021-04-23 23:23:52 -0500

    updated index

--- script.js
+++ script.js
@@ -21,3 +21,13 @@ for ( var i=0, len = draggableElems.length; i < len; i++ ) {
   });
   draggies.push( draggie );
 }
+
+  
+function myFunction1(elem) {
+    var x = document.getElementById(elem);
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
