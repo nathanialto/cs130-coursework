11dc19121975f9da21b166c685ecfb621cf8a967
nathanialto <67659961+nathanialto@users.noreply.github.com>
2021-04-29 16:28:59 -0500

    adding tutorial05

---
+++ tutorial05/theme-switcher/js/index.js
@@ -0,0 +1,20 @@
+const defaultTheme = () => {
+   document.querySelector('body').className = 'container'
+   alert('switch to default theme');
+};
+
+const oceanTheme = () => {
+   document.querySelector('body').className = 'container ocean'
+
+};
+
+const desertTheme = () => {
+   document.querySelector('body').className = 'container desert'
+
+};
+
+
+document.querySelector('#default').onclick = defaultTheme;
+document.querySelector('#ocean').onclick = oceanTheme;
+document.querySelector('#desert').onclick = desertTheme;
+
