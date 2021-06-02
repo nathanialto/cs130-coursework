e5067ac3eb63123eda25157499b87bf69c3d3f1e
nathanialto <67659961+nathanialto@users.noreply.github.com>
2021-06-02 16:28:36 -0500

    fixed images?

--- project/script.js
+++ project/script.js
@@ -1,12 +1,12 @@
 let width=700, height=500, X = width/2-5, Y = height/2-5, objectwidth = 60;
 let X1, Y1, X2, Y2, X3, Y3, X4, Y4, X5, Y5, icon, anxiety, skateboard, skunk, dance_mar, canadian_goose;
 function preload() {
-  icon = loadImage("images/head.png");
-  anxiety = loadImage('images/anxiety.png');
-  skateboard = loadImage('images/skateboard.png');
-  dance_mar = loadImage('images/dance_mar.png');
-  canadian_goose = loadImage('images/canadian_goose.png');
-  skunk = loadImage('images/skunk.png');
+  icon = loadImage("images/head.PNG");
+  anxiety = loadImage('images/anxiety.PNG');
+  skateboard = loadImage('images/skateboard.PNG');
+  dance_mar = loadImage('images/dance_mar.PNG');
+  canadian_goose = loadImage('images/canadian_goose.PNG');
+  skunk = loadImage('images/skunk.PNG');
 
 }
 function setup() {
@@ -51,12 +51,12 @@ function draw() {
   // circle(X4, Y4, objectwidth); 
   // circle(X5, Y5, objectwidth); 
 
-  image(icon, X, Y, objectwidth);
-  image(anxiety, X1, Y1, objectwidth); 
-  image(skateboard, X2, Y2, objectwidth); 
-  image(dance_mar, X3, Y3, objectwidth); 
-  image(canadian_goose, X4, Y4, objectwidth); 
-  image(skunk, X5, Y5, objectwidth); 
+  image(icon, X, Y, objectwidth, objectwidth);
+  image(anxiety, X1, Y1, objectwidth, objectwidth); 
+  image(skateboard, X2, Y2, objectwidth, objectwidth); 
+  image(dance_mar, X3, Y3, objectwidth, objectwidth); 
+  image(canadian_goose, X4, Y4, objectwidth, objectwidth); 
+  image(skunk, X5, Y5, objectwidth, objectwidth); 
 
   
   if (X==X1 && Y==Y1) {
